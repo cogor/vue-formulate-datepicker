bbc24396cd1f1ea0938e9d6e8a94a5b719dc3c40
Ilya Artamonov <ilya.sosidka@gmail.com>
2021-05-24 20:06:45 +0300

    Fix language import

--- src/VueFormulateDatepicker.vue
+++ src/VueFormulateDatepicker.vue
@@ -6,14 +6,13 @@
     v-model="date"
     :options="context.options"
     v-bind="context.attributes"
-    :language="languages[context.attributes.language]"
+    :language="language"
     @blur="context.blurHandler"
   />
 </template>
 
 <script>
 import Datepicker from "@sum.cumo/vue-datepicker";
-import * as lang from "@sum.cumo/vue-datepicker/dist/locale/index.esm";
 
 export default {
   name: "VueFormulateDatapicker",
@@ -25,18 +24,31 @@ export default {
   },
   data() {
     return {
-      languages: lang,
+      language: {},
       date: null,
     };
   },
   watch: {
     date() {
       let d = this.date;
-      let year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
-      let month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
-      let day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
-      this.context.model = `${year}-${month}-${day}`
-    }
+      let year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
+      let month = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(d);
+      let day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
+      this.context.model = `${year}-${month}-${day}`;
+    },
+  },
+  methods: {
+    getLanguage() {
+      let lang = this.context.attributes.language ?? "en";
+      import(
+        `@sum.cumo/vue-datepicker/dist/locale/translations/${lang}.js`
+      ).then((result) => {
+        this.language = result.default;
+      });
+    },
+  },
+  mounted() {
+    this.getLanguage();
   },
   components: { Datepicker },
 };
